2e8c758dfe6557cadb5901eaeacb237673d58784
Kim Yong Lim <kimyong@Kims-MacBook-Pro.local>
2019-07-29 22:48:22 +0800

    Updates

---
+++ precache-manifest.b717bde159eccf724fedf17ac0eee211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5332951dd02b57d1920c91eb1dee092c",
+    "url": "/CatWorld/index.html"
+  },
+  {
+    "revision": "72bce7a6e5e7b68ca420",
+    "url": "/CatWorld/static/css/main.89f46d62.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/CatWorld/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "72bce7a6e5e7b68ca420",
+    "url": "/CatWorld/static/js/main.976fe5b9.chunk.js"
+  },
+  {
+    "revision": "c2ca338e296ad965dbd0",
+    "url": "/CatWorld/static/js/runtime~main.332d61d0.js"
+  }
+]);
